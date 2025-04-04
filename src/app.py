824647af915ae7409f5f65faf707c9885f3f962d
Kirtan Soni <118914256+sonikirtan110@users.noreply.github.com>
2025-04-04 21:57:52 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -54,5 +54,5 @@ def predict():
     return jsonify({"predicted_quality": result})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=True)
